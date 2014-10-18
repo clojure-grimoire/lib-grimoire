6407e6ca732d4a1a7da15e0f45eba81e39e7f421
arrdem <me@arrdem.com>
2014-10-18 18:30:10 -0500

    Initial state

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to grimoire.lib
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/grimoire/lib_test.clj
@@ -0,0 +1,7 @@
+(ns grimoire.lib-test
+  (:require [clojure.test :refer :all]
+            [grimoire.lib :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
