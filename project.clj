c994015b3e17dd85b8b7ec51baf5bfe2391fa825
arrdem <me@arrdem.com>
2014-10-18 18:33:17 -0500

    Fixup project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject grimoire.lib "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject lib-grimoire "0.1.0-SNAPSHOT"
+  :description "A shared library for Grimoire infrastructure"
+  :url "http://github.com/clojure-grimoire/lib-grimoire"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [grimradical/clj-semver "0.3.0-SNAPSHOT"]])
