6ddee38c45f3d2c897700270017fd5701cb7d4ea
arrdem <me@arrdem.com>
2014-12-05 01:59:46 -0600

    Start working on adding tests of the fs

---
+++ test/grimoire/api/fs_test.clj
@@ -0,0 +1,31 @@
+(ns grimoire.api.fs-test
+  (:require [grimoire.api :as api]
+            [grimoire.things :as t]
+            [grimoire.api.fs]
+            [grimoire.api.fs.read]
+            [grimoire.api.fs.write]
+            [clojure.test :refer :all]))
+
+(def test-config
+  {:datastore
+   {:docs  "resources/test/docs/"
+    :notes "resources/test/notes/"
+    :mode  :filesystem}})
+
+(deftest list-groups-test
+  (is (= (api/list-groups test-config)
+         (list {:type :group, :parent nil, :name "org.bar", :uri "org.bar"}
+               {:type :group, :parent nil, :name "org.foo", :uri "org.foo"}))))
+
+(deftest list-artifacts-test
+  (let [g       (t/->Group "org.foo")
+        members (api/list-artifacts test-config g)]
+    (is (= ["a" "b"]
+           (sort (map :name members))))))
+
+(deftest list-versions-test
+  (let [g        (t/->Group "org.foo")
+        a        (t/->Artifact g "a")
+        versions (api/list-versions test-config a)]
+    (is (= ["0.1.0" "0.1.0-SNAPSHOT" "1.0.0" "1.0.1" "1.1.0"]
+           (sort (map :name versions))))))
