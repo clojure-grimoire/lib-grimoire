96f5012aa08ef037c37bf4369484125d52969edc
arrdem <me@arrdem.com>
2014-12-23 15:16:41 -0600

    Include lein-grim in :dev

--- project.clj
+++ project.clj
@@ -5,4 +5,10 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [version-clj "0.1.0"]]
-  :profiles {:dev {:dependencies [[org.clojure/test.check "0.6.1"]]}})
+  :profiles {:dev {:dependencies [[org.clojure/test.check "0.6.1"]
+                                  [org.clojure-grimoire/lein-grim "[0.3.2,)"]]
+                   :aliases {"grim" ["run" "-m" "grimoire.doc"
+                                     ,,:project/groupid
+                                     ,,:project/artifactid
+                                     ,,:project/version
+                                     ,,nil]}}})
