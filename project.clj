ef5403786416430eb8df7e57f49c13ce5e244044
arrdem <me@arrdem.com>
2015-02-15 23:13:51 -0600

    SQUASHME use latest detritus

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [version-clj "0.1.0"]
-                 [me.arrdem/detritus "0.2.1"]]
+                 [me.arrdem/detritus "0.2.2-SNAPSHOT"]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.6.1"]
                                   [org.clojure-grimoire/lein-grim "[0.3.2,)"]]
                    :aliases {"grim" ["run" "-m" "grimoire.doc"
