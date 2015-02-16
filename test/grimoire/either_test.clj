d177401ddf3de0cdf31e7cfd26457e430e77d822
arrdem <me@arrdem.com>
2015-02-16 01:53:39 -0600

    M0ar tests

--- test/grimoire/either_test.clj
+++ test/grimoire/either_test.clj
@@ -12,3 +12,17 @@
 
     (is (either/either? (either/succeed r)))
     (is (either/either? (either/fail r)))))
+
+(deftest simple-with-result-test
+  (is (= \h
+         (either/with-result [x (either/succeed "hax! I call hax!")]
+           (first x)))))
+
+(deftest complex-with-result-test
+  (let [f (fn [x]
+            (either/with-result [a (x)]
+              (either/succeed (inc a))
+              (either/fail (str "Nested falure: " a " In fn " x))))]
+    (is (= (either/succeed 2)
+           (f #(either/succeed 1))))
+    (is (either/fail? (f #(either/fail "Fuck off!"))))))
