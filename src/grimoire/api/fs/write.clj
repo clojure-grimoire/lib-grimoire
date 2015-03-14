4b76a7e7f8e9fb4d57d6b4fb6aa80fc67302f447
arrdem <me@arrdem.com>
2015-03-14 03:21:25 -0500

    Implement -write-example for FS

--- src/grimoire/api/fs/write.clj
+++ src/grimoire/api/fs/write.clj
@@ -31,7 +31,18 @@
     (.mkdirs (.getParentFile handle))
     (spit handle data)))
 
-;; FIXME: add write-example
+(defmethod api/-write-example ::fs/Config [config thing data]
+  {:pre [(fs/Config? config)
+         (t/thing? thing)
+         (string? data)]}
+  (let [thing  (t/ensure-thing thing)
+        _      (assert thing)
+        id     (java.util.UUID/randomUUID)
+        handle (impl/thing->example-handle config thing id)
+        _      (assert thing)]
+    (.mkdirs (.getParentFile handle))
+    (spit handle data)))
+
 
 (defmethod api/-write-related ::fs/Config [config thing related-things]
   {:pre [(fs/Config? config)
