983946bf1f7fd5d4ddf23a2a4b0091060b1eafc0
arrdem <me@arrdem.com>
2015-03-14 09:37:16 -0500

    Bump

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject org.clojure-grimoire/lib-grimoire "0.8.0-ALPHA1"
+(defproject org.clojure-grimoire/lib-grimoire "0.8.0"
   :description "A shared library for Grimoire infrastructure"
   :url "http://github.com/clojure-grimoire/lib-grimoire"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-alpha4"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [version-clj "0.1.0"
                   :exclusions [org.clojure/clojure]]
-                 [me.arrdem/detritus "0.2.2-SNAPSHOT"
+                 [me.arrdem/detritus "0.2.2"
                   :exclusions [org.clojure/clojure]]]
   :profiles {:dev {:dependencies [[org.clojure/test.check "0.6.1"
                                    :exclusions [org.clojure/clojure]]]}})
