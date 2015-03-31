63475e795f366b7d36090e3d522765313d66fa8e
arrdem <me@arrdem.com>
2015-03-30 19:15:03 -0500

    Bumped version to 0.9.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojure-grimoire/lib-grimoire "0.9.0"
+(defproject org.clojure-grimoire/lib-grimoire "0.9.1"
   :description "A shared library for Grimoire infrastructure"
   :url "http://github.com/clojure-grimoire/lib-grimoire"
   :license {:name "Eclipse Public License"
